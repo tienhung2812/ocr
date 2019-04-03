8fed2eb7dcb438ac77633edb18224bd60b3a4d59
hung.nguyen <hung.nguyen@edge-works.net>
2019-04-03 15:55:29 +0700

    Replace eng and vie with higher conf

--- xml/pdf2xml.py
+++ xml/pdf2xml.py
@@ -6,7 +6,11 @@ except ImportError:
 import io 
 import pytesseract
 import sys
+from bs4 import BeautifulSoup
 
+CONFIG={
+    'low_conf':90
+}
 class Pdf2Xml:
     def __init__(self,file, lang='eng'):
         self.pdfPath = file
@@ -18,11 +22,59 @@ class Pdf2Xml:
                 img.format = 'png'
                 img.save(transfer)
             with pilImg.open(transfer) as img:
-                return pytesseract.image_to_pdf_or_hocr(img, extension='hocr',lang=self.lang)
-                # return pytesseract.image_to_data(img,lang=self.lang)
+                return self.runtesseract(img)
+                # return pytesseract.image_to_pdf_or_hocr(img, extension='hocr',lang=self.lang)
+    def runtesseract(self,img):
+        eng = pytesseract.image_to_pdf_or_hocr(img, extension='hocr',lang='eng')
+        vie = pytesseract.image_to_pdf_or_hocr(img, extension='hocr',lang='vie')
+
+        return self.compareConf(vie,eng)
+    
+    def extractAttr(self,data):
+        info = data['title'].split("x_wconf ")
+        box = info[0]
+        conf = int(info[1])
+        return box, conf
+
+    def extractSpanData(self,data):
+        result = []
+        for span in data.find_all('span'):
+            if len(span.contents) <= 1:
+                box, conf = self.extractAttr(span)
+                string = span.string
+                result.append({
+                    'string':string,
+                    'box': box,
+                    'conf':conf
+                })
+        return result
+
+    def compareConf(self,vie,eng):
+
+        viesoup = BeautifulSoup(vie,'xml')
+        engsoup = BeautifulSoup(eng,'xml')
+
+        engdata = self.extractSpanData(engsoup)
+
+        for span in viesoup.find_all('span'):
+            if len(span.contents) <= 1:
+                box,conf = self.extractAttr(span)
+                #if object have low confident
+                if conf <= CONFIG['low_conf']:
+                    for e in engdata:
+                        if box == e['box']:
+                            if e['conf']> conf and e['conf'] >=CONFIG['low_conf']:
+                                span.string.replaceWith(e['string'])
+                                span['conf'] = e['conf']
+                                # print('{} {}'.format(span.string,e['string']))
+                    
+
+        return viesoup.prettify()
+        
+
 
 if len(sys.argv) <= 1:
-    a = Pdf2Xml(file = 'stock/2.pdf',lang='vie')
+    a = Pdf2Xml(file = 'stock/don-thuoc.png',lang='vie')
     text_file = open("Output.hocr", "wb")
     output = a.execute()
     print(output)
@@ -42,7 +94,7 @@ else:
     if outputfile is None:
         print(output)
     else:
-        text_file = open(outputfile, "wb")
+        text_file = open(outputfile, "w")
         # output = a.execute()
         # print(output)
         text_file.write(output)
