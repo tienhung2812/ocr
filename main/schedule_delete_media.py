f6ee67a3f0704552931a97684c43bd97adf78a31
Nguyen Tien Hung <hung.nguyentien97>
2019-07-23 23:49:13 +0700

    reuse everything

--- main/schedule_delete_media.py
+++ main/schedule_delete_media.py
@@ -5,16 +5,19 @@ import shutil
 import time
 import datetime
 import django
-
+import logging
 import schedule
 from ocr.settings import SCHEDULE_DELETE_MEDIA
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ocr.settings")
 django.setup()
 
 
 def delete_media_file():
-    print("Checking at ",datetime.datetime.now())
+    logger.info("Checking at %s"%str(datetime.datetime.now()))
     folder = os.getcwd() + '/media'
     for the_file in os.listdir(folder):
         file_path = os.path.join(folder, the_file)
@@ -23,10 +26,10 @@ def delete_media_file():
                 os.unlink(file_path)
             else:
                 shutil.rmtree(file_path)
-            print("Deleted: ",file_path)
+            logger.info("Deleted: %s"%str(file_path))
             #elif os.path.isdir(file_path): shutil.rmtree(file_path)
         except Exception as e:
-            print(e)
+            logger.error(e)
 
 if __name__ == "__main__":
     schedule.every(SCHEDULE_DELETE_MEDIA).minutes.do(delete_media_file)
