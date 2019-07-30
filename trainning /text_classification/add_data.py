18317dd344b86508011cdd0f806cac8f7af779be
Nguyen Tien Hung <hung.nguyentien97>
2019-07-30 23:31:41 +0700

    Don't know what i am doing :((

--- trainning /text_classification/add_data.py
+++ trainning /text_classification/add_data.py
@@ -82,4 +82,120 @@ def check_text(start = 0, check_col_null = True):
     df.to_csv(path, index = False,  encoding='utf-8')
     print("Done !")
 
-check_text(0)
+def append_csv(file_name,row,lines_array):
+    for line in lines:
+        df = pd.read_csv(file_name,   encoding='utf-8')
+        length = df.shape[0]
+
+        data = {
+            "sentence":line.replace("\n",""),
+        }
+
+        y_col = ["brand_name","info","index","content","total","thank_you"]
+
+        for col in y_col:
+            if row == col:
+                data[col] = 1
+            else:
+                data[col] = 0
+        
+        df.loc[length] = data
+        df.to_csv(file_name, index = False,  encoding='utf-8')
+
+# check_text(0)
+# dataa = ""
+# with open('info_data', 'r') as f:
+#     lines = f.readlines()
+#     for line in lines:
+#         arrr = line.split(' ')
+#         stop = 0
+#         for i,word in enumerate(arrr):
+#             try:
+#                 num = int(word)
+#                 if num > 0:
+#                     stop = i
+#                     break
+#             except:
+#                 pass
+
+#         if stop == 0 :
+#             stop = len(arrr) -1
+#         dataa+=' '.join(arrr[:stop])+'\n'
+# with open('info_data', 'w') as f:
+#     f.write(dataa)
+
+# Append data
+# df_col = ["sentence","brand_name","info","index","content","total","thank_you"]
+# y_col = ["brand_name","info","index","content","total","thank_you"]
+# with open('brand_data', 'r') as f:
+#     lines = f.readlines()
+#     append_csv('30-07-data-vi.csv',"brand_name",lines)
+
+# Generate index data
+import random
+def generate_random_index(array):
+    return array[random.randint(0,len(array)-1)]+' '
+
+def random_yn():
+    tt = random.randint(0,1)
+    if tt == 0:
+        return False
+    else:
+        return True
+
+# mat_hang = ["Mặt hàng","Tên hàng", "TÊN", "HÀNG", "TÊN HÀNG","THỨC UỐNG","ĐỒ ĂN"," MÓN HÀNg","Món hàng"]
+# stt = ["STt","STT","Stt"]
+# sl = ['sl',"SL","Sl"]
+# dgia = ["DGia","Đ.Giá","ĐGía", "Đơn Giá"]
+# ttien = ["T.Tiền","T.TIỀN","Thành Tiền"]
+# import random
+# dataa = ''
+# for i in range(0,100):
+#     if random_yn():
+#         dataa+= generate_random_index(sl)
+#     dataa += generate_random_index(mat_hang)
+#     dataa += generate_random_index(sl)
+#     dataa += generate_random_index(dgia)
+#     if random_yn():
+#         dataa+= generate_random_index(ttien)
+#     dataa += '\n'
+# with open('index_data', 'w') as f:
+#     f.write(dataa)
+
+# Append data
+# df_col = ["sentence","brand_name","info","index","content","total","thank_you"]
+# y_col = ["brand_name","info","index","content","total","thank_you"]
+# with open('index_data', 'r') as f:
+#     lines = f.readlines()
+#     append_csv('30-07-data-vi.csv',"index",lines)
+
+
+# THANK YOU
+# hen_gap_lai = ["Hẹn gặp lại",
+#             "HEN GAP LAI",
+#             "HẸN GẶP LẠI"]
+# thank_you = ["Cám ơn quý khách",
+#             "Thank you",
+#             "CAM ON",
+#             "CAM ƠN QUÝ KHÁCH",
+#             "Cam on"]
+# dataa = ''
+# for i in range(0,70):
+#     newl = False
+#     if random_yn():
+#         dataa += generate_random_index(thank_you)+"."
+#         newl = True
+#     if random_yn():
+#         dataa += generate_random_index(hen_gap_lai)
+#         newl = True
+#     if newl:
+#         dataa += '\n'
+# with open('thank_you_data', 'w') as f:
+#     f.write(dataa)
+
+# Append data
+df_col = ["sentence","brand_name","info","index","content","total","thank_you"]
+y_col = ["brand_name","info","index","content","total","thank_you"]
+with open('thank_you_data', 'r') as f:
+    lines = f.readlines()
+    append_csv('30-07-data-vi.csv',"thank_you",lines)
